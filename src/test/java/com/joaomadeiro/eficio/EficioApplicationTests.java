93a874e97e3a4d1d627d7daa694de1aa56d6f428
JoaoVitorMadeiro <joao.madeirodev@gmail.com>
2025-03-12 11:54:10 -0300

    First Commit

---
+++ src/test/java/com/joaomadeiro/eficio/EficioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaomadeiro.eficio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EficioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
